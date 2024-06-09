87cfaafa71f13142687449a1cf31a1eacd5fc177
Kaushik0126 <126869954+Kaushik0126@users.noreply.github.com>
2024-06-09 12:47:42 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Chatbot from './Chatbot';
+import Navbar from './Navbar';
+function App() {
+  return (
+    <>
+        <Navbar />
+        <Chatbot />
+    </>
+  )
+}
+
+export default App;
